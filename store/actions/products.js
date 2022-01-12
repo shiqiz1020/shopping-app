8e5281c4b9918003cf628661fd5a36f81a3b7aae
Shiqi Zhang <shiqiz1020@gmail.com>
2022-01-11 21:43:03 -0800

    handling errors

--- store/actions/products.js
+++ store/actions/products.js
@@ -44,10 +44,14 @@ export const fetchProducts = () => {
 
 export const deleteProduct = productId => {
   return async dispatch => {
-    await fetch(
+    const response = await fetch(
       `https://shopping-app-1cad5-default-rtdb.firebaseio.com/products/${productId}.json`,
       {method: 'DELETE'}
     );
+
+    if (!response.ok) {
+      throw new Error('something went wrong!');
+    }
 
     dispatch({ type: DELETE_PRODUCT, pid: productId });
   }
@@ -89,7 +93,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 
 export const updateProduct = (id, title, description, imageUrl) => {
   return async dispatch => {
-    await fetch(
+    const response = await fetch(
       `https://shopping-app-1cad5-default-rtdb.firebaseio.com/products/${id}.json`,
       {
         method: 'PATCH',
@@ -113,5 +117,9 @@ export const updateProduct = (id, title, description, imageUrl) => {
         imageUrl
       }
     });
+
+    if (!response.ok) {
+      throw new Error('something went wrong!');
+    }
   }
 };
