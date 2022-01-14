5237a70dfa7a57b368eff0d6bbb17aef2b81d4cb
Shiqi Zhang <shiqiz1020@gmail.com>
2022-01-13 18:20:37 -0800

    mapped orders to users

--- store/actions/products.js
+++ store/actions/products.js
@@ -6,8 +6,8 @@ export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export const fetchProducts = () => {
-  return async dispatch => {
-    // any async code you want!
+  return async (dispatch, getState) => {
+    const userId = getState().auth.userId;
     try {
       const response = await fetch(
         'https://shopping-app-1cad5-default-rtdb.firebaseio.com/products.json'
@@ -33,7 +33,11 @@ export const fetchProducts = () => {
         );
       }
   
-      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+      dispatch({ 
+        type: SET_PRODUCTS, 
+        products: loadedProducts,
+        userProducts: loadedProducts.filter(prod => prod.ownerId === userId)
+      });
     } catch (err) {
       // send to custom analytics server
       throw err;
@@ -60,6 +64,7 @@ export const deleteProduct = productId => {
 export const createProduct = (title, description, imageUrl, price) => {
   return async (dispatch, getState) => {
     const token = getState().auth.token;
+    const userId = getState().auth.userId;
     const response = await fetch(
       `https://shopping-app-1cad5-default-rtdb.firebaseio.com/products.json?auth=${token}`,
       {
@@ -71,7 +76,8 @@ export const createProduct = (title, description, imageUrl, price) => {
           title,
           description,
           imageUrl,
-          price
+          price,
+          ownerId: userId
         })
       }
     );
@@ -85,7 +91,8 @@ export const createProduct = (title, description, imageUrl, price) => {
         title,
         description,
         imageUrl,
-        price
+        price,
+        ownerId: userId
       }
     });
   };
